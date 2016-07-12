cf63bd9d8030f8179e425e0e8100c614b5f97fe2
ayoub_barakat <ayoub1992fst@gmail.com>
2016-07-12 14:02:33 +0100

    saving changes

--- config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by elouai on 23/02/16.
- */
-module.exports = {
-    secret: 'stmHH12Wt345.aFV3w3wi',
-    email : 'stm.node@gmail.com',
-    password : 'randPASSWORD2016'
-}
--- db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/stm', function () {
-    console.log('mongodb connected')
-})
-module.exports = mongoose
