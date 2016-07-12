2486f481f916fed3fd992989bffa824278979390
ayoub_barakat <ayoub1992fst@gmail.com>
2016-07-12 16:13:18 +0100

    my resume well done

--- public/config.js
+++ public/config.js
@@ -7,7 +7,7 @@ function config($urlRouterProvider, $stateProvider) {
               .state('formation',   {url: '/formation', controller: 'Ctrl', templateUrl: 'templates/formation.tmpl.html' })
               .state('experience',   {url: '/experience', controller: 'Ctrl', templateUrl: 'templates/experience.tmpl.html' })
               .state('competence',   {url: '/competence', controller: 'Ctrl', templateUrl: 'templates/competence.tmpl.html' })
-              .state('projet',   {url: '/projet', controller: 'Ctrl', templateUrl: 'templates/projet.tmpl.html' })
+              .state('projet',   {url: '/projet', controller: 'Ctrl', templateUrl: 'templates/project.tmpl.html' })
               .state('language',   {url: '/language', controller: 'Ctrl', templateUrl: 'templates/language.tmpl.html' })
               .state('other',   {url: '/other', controller: 'Ctrl', templateUrl: 'templates/other.tmpl.html' })
               .state('para-universitaire',   {url: '/para-universitaire', controller: 'Ctrl', templateUrl: 'templates/para-universitaire.tmpl.html' })
